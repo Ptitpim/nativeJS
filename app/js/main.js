5d8c18af830b33a9a46ca08760405e1cbf082d76
Ptitpim <francois.pouthier@gmail.com>
2015-01-11 00:32:51 +0100

    Add HTML node after an other node

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,38 @@
     vnItemsTpl = _.template(document.querySelector('#vn-items-tpl').innerHTML);
     vnItemsContainer = document.querySelector('.vn-item-container');
 
+    insertTag();
+
     getCars();
   });
+
+  function insertTag() {
+    var h1 = document.querySelector('h1'),
+        tag = document.createElement('p');
+
+    tag.innerText = 'Example of added node';
+    insertAfter(tag, h1);
+  }
+
+  /**
+   * Insert new node after element
+   *
+   * @param newElement The new node
+   * @param targetElement The target node
+   */
+  function insertAfter(newElement, targetElement) {
+    // Target is what you want it to go after. Look for this elements parent.
+    var parent = targetElement.parentNode;
+
+    // If the parents lastchild is the targetElement...
+    if (parent.lastchild == targetElement) {
+      // Add the newElement after the target element.
+      parent.appendChild(newElement);
+    } else {
+      // Else the target has siblings, insert the new element between the target and it's next sibling.
+      parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+  }
 
   // Get cars from json file
   function getCars() {
